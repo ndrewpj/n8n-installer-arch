fa0c71e5d1ace8f7fc9df623240dc4b86e21c418
ndrewpj <_apj@mail.ru>
2025-12-15 21:17:43 +0300

    go5 caddy fix

--- scripts/03_generate_secrets.sh
+++ scripts/03_generate_secrets.sh
@@ -101,10 +101,10 @@ if [ -f "$OUTPUT_FILE" ]; then
 fi
 
 # Install Caddy
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-apt install -y caddy
-
+#curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+#curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+#apt install -y caddy
+pacman -Sy --noconfirm caddy
 # Check for caddy
 if ! command -v caddy &> /dev/null; then
     log_error "caddy could not be found. Please ensure it is installed and available in your PATH." >&2
